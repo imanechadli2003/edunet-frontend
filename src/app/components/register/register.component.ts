303cbd156c89628332f45226348d7ba94528f28e
Imane <imanechadli20003@gmail.com>
2024-05-28 23:29:15 +0100

    make branch field of selection type

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import {BranchService} from "../../services/branch.service";
+import {Branch} from "../../shared/data/branch";
 
 
 @Component({
@@ -12,12 +14,16 @@ import { UserService } from '../../services/user.service';
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
-  
+export class RegisterComponent implements OnInit {
+
   registerForm: FormGroup;
 
   rtr: Router;
-  
+
+  branchService = inject(BranchService);
+
+  branchList: Branch[] = [];
+
   constructor(private userService: UserService, private fb: FormBuilder, private router: Router) {
     this.rtr = router;
     this.registerForm = fb.group({
@@ -41,5 +47,13 @@ export class RegisterComponent {
     } else {
       console.log(this.registerForm.errors);
     }
+  }
+
+  ngOnInit(): void {
+    this.branchService.getBranches().subscribe(
+      (res: Branch[]) => {
+        this.branchList = res;
+      }
+    )
   }
 }
